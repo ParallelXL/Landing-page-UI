1994b1344b193e8654dadddf8cd719d36fa9e246
ParallelXL <ayanmsn07@gmail.com>
2023-09-05 14:00:27 +0530

    added mobile navigation toggle

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 "use strict";
 
 const navMobile = document.querySelector(".nav-mobile");
-const menuIcon = document.querySelector(".menu");
+const menuIcon = document.querySelector(".bx-menu");
+
+menuIcon.addEventListener("click", (e) => {
+  e.preventDefault();
+  navMobile.classList.toggle("active");
+});
