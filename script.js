c7e5292ab00c18cff44894d64b6bca646022442d
bello yahya <belloy4606@gmail.com>
2023-09-07 12:16:12 +0100

    added a sun icon for darkmode

--- script.js
+++ script.js
@@ -10,9 +10,19 @@ menuIcon.addEventListener("click", (e) => {
 });
 
 const switchBtn = document.querySelector(".switch");
-const toggler = document.getElementById("toggler");
-let darkMode = 0;
+const moon = document.getElementById("moonIcon");
+const sun = document.getElementById("sunIcon");
+let darkMode = false;
 
 switchBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
+  darkMode = 1 - darkMode;
+
+  if (darkMode) {
+    moon.style.display = "none";
+    sun.style.display = "block";
+  } else {
+    moon.style.display = "block";
+    sun.style.display = "none";
+  }
 });
